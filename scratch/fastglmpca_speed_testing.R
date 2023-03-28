23461251fab99a97470b7980be83050b6e54e925
Eric Weine <ericweine15@gmail.com>
2023-03-28 18:57:42 -0400

    adding scratch before branch switch

--- scratch/fastglmpca_speed_testing.R
+++ scratch/fastglmpca_speed_testing.R
@@ -1,6 +1,6 @@
 # here, want to design a test of computational speed for glmpca data
 
-n_cells <- c(1000, 2500, 5000, 10000, 25000)
+n_cells <- c(1000)
 n_genes <- 2 * n_cells
 
 time_vec <- c()
@@ -8,23 +8,24 @@ loglik_vec <- c()
 
 for (i in 1:length(n_cells)) {
   
-  set.seed(n_cells)
+  set.seed(n_cells[i])
   sim_data <- plash::generate_glmpca_data(
-    n = n_genes, p = n_cells, K = 10
+    n = n_genes[i], p = n_cells[i], K = 8
   )
   
-  sim_data$Y <- sim_data$Y[rowSums(data$Y) > 0,]
-  sim_data$Y <- sim_data$Y[, colSums(data$Y) > 0]
+  sim_data$Y <- sim_data$Y[rowSums(sim_data$Y) > 0,]
+  sim_data$Y <- sim_data$Y[, colSums(sim_data$Y) > 0]
   
   set.seed(1)
   
-  tictoc::tic()
   fit0 <- plash::init_glmpca(
-    Y = sim_data$Y, K = 10, fit_col_size_factor = TRUE, fit_row_intercept = TRUE
+    Y = sim_data$Y, K = 8, fit_col_size_factor = TRUE, fit_row_intercept = TRUE
   )
+  tictoc::tic()
+  fit <- plash::fit_glmpca(Y = sim_data$Y, fit0 = fit0, control = list(line_search = TRUE, num_iter = 5))
   stop <- tictoc::toc()
   time_elapsed <- stop$toc - stop$tic
-  loglik <- tail(fit0$progress$loglik, 1)
+  loglik <- tail(fit$progress$loglik, 1)
   
   time_vec <- c(time_vec, time_elapsed)
   loglik_vec <- c(loglik_vec, loglik)
