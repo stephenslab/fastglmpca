818bbf768105a95e6ffd1ef4596165f379f4b9d3
Eric Weine <ericweine15@gmail.com>
2023-04-19 12:55:16 -0400

    added pbmc experiment files

---
+++ inst/scratch/fit_pbmc_glmpca.R
@@ -0,0 +1,70 @@
+command_args = commandArgs(trailingOnly = TRUE)
+n_factor = as.integer(command_args[1])
+n_iter = as.integer(command_args[2])
+optimizer = as.character(command_args[3])
+minibatch = as.character(command_args[4])
+
+print(optimizer)
+print(minibatch)
+
+load("/project2/mstephens/pcarbo/git/fastTopics-experiments/data/pbmc_68k.RData")
+#load("~/Documents/plash/scratch/droplet.RData")
+
+if (optimizer == "fisher") {
+  
+  data <- as.matrix(counts)
+  
+}
+
+
+set.seed(1)
+if (optimizer == "fisher") {
+  
+  fit0 <- plash::init_glmpca(
+    Y = data, K = n_factor, fit_col_size_factor = TRUE, fit_row_intercept = TRUE
+  )
+  
+} else {
+  
+  fit0 <- plash::init_glmpca(
+    Y = counts, K = n_factor, fit_col_size_factor = TRUE, fit_row_intercept = TRUE
+  )
+  
+}
+
+
+library(tictoc)
+
+tic()
+if(optimizer == "fisher") {
+  
+  fit <- glmpca::glmpca(
+    Y = data,
+    L = n_factor,
+    fam = "poi",
+    optimizer = optimizer,
+    minibatch = minibatch,
+    ctl = list(minIter = floor(n_iter / 2), maxIter = n_iter, verbose = TRUE, tol = .Machine$double.eps),
+    init = list(factors = t(fit0$FF[-c(1,2),]), loadings = t(fit0$LL[-c(1,2),]))
+  )
+  
+} else {
+  
+  fit <- glmpca::glmpca(
+    Y = counts,
+    L = n_factor,
+    fam = "poi",
+    optimizer = optimizer,
+    minibatch = minibatch,
+    ctl = list(minIter = floor(n_iter / 2), maxIter = n_iter, verbose = TRUE, tol = .Machine$double.eps),
+    init = list(factors = t(fit0$FF[-c(1,2),]), loadings = t(fit0$LL[-c(1,2),]))
+  )
+  
+}
+
+toc()
+
+readr::write_rds(
+  fit, 
+  glue::glue("pbmc_glmpca_fit_{n_factor}_factors_{n_iter}_iter_{optimizer}_optimizer_minibatch_{minibatch}.rds")
+)
---
+++ inst/scratch/fit_pbmc_plash.R
@@ -0,0 +1,31 @@
+command_args = commandArgs(trailingOnly = TRUE)
+n_factor = as.integer(command_args[1])
+n_iter = as.integer(command_args[2])
+n_cores = as.integer(command_args[3])
+
+load("/project2/mstephens/pcarbo/git/fastTopics-experiments/data/pbmc_68k.RData")
+#load("~/Documents/plash/scratch/droplet.RData")
+
+set.seed(1)
+fit0 <- plash::init_glmpca(
+  Y = counts, K = n_factor, fit_col_size_factor = TRUE, fit_row_intercept = TRUE
+)
+
+library(tictoc)
+tic()
+fit <- plash::fit_glmpca(
+  Y = counts, 
+  fit0 = fit0, 
+  algorithm = "ccd", 
+  link = "log",
+  control = list(line_search = TRUE, num_iter = 3, alpha = .1),
+  warmup = TRUE, 
+  max_iter = n_iter,
+  tol = .Machine$double.eps
+)
+toc()
+
+readr::write_rds(
+  fit, 
+  glue::glue("pbmc_plash_fit_{n_cores}_cores_{n_factor}_factors_{n_iter}_iter.rds")
+)
