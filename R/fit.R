9361f057c6e59eff8147bd79a02a6af8cc631003
Eric Weine <ericweine15@gmail.com>
2023-07-11 10:43:03 -0500

    added approximation method proposed by PCarbo

--- R/fit.R
+++ R/fit.R
@@ -56,121 +56,12 @@ lik_glmpca_pois_log1p <- function(Y, LL, FF, const) {
   
 }
 
-#' @title Fit Poisson GLM-PCA Model to Count Data
-#' 
-#' @description Fit a Poisson GLM-PCA model to input matrix \code{Y}
-#'   by maximum likelihood.
-#'   
-#' @details In generalized principal component analysis (GLM-PCA)
-#' based on a Poisson likelihood (Townes et al, 2019), the counts
-#' \eqn{y_{ij}} in the n x p matrix \eqn{Y} are modeled as
-#' \deqn{y_{ij} \sim Poisson(\lambda_{ij}).} The logarithm of each
-#' Poisson rate is defined as a linear combination of the parameters:
-#' \deqn{\log \lambda_{ij} = \sum_{k=1}^K l_{ki} f_{kj} = (L'F)_{ij}.} The model
-#' parameters are stored as an K x n matrix \eqn{L} with entries
-#' \eqn{l_{ki}} and an K x p matrix \eqn{F} with entries \eqn{f_{kj}}.
-#' \eqn{K} is a tuning parameter specifying the rank of the matrices
-#' \eqn{L} and \eqn{F}. \code{fit_glmpca_pois} computes maximum-likelihood
-#' estimates (MLEs) of \eqn{L} and \eqn{F}.
-#' 
-#' The algorithm works by repeatedly alternating between updating \eqn{L} with
-#' \eqn{F} fixed and updating \eqn{F} with \eqn{L} fixed. Each update takes
-#' the form of a series of Poisson regressions solved using cyclic co-ordinate
-#' descent (ccd).
-#' 
-#' The \code{control} argument is a list in which any of the following
-#' named components will override the default optimization algorithm
-#' settings (as they are defined by \code{fit_glmpca_control_default}):
-#' 
-#' \describe{
-#'
-#' \item{\code{num_iter}}{Number of ccd updates to be made to parameters
-#'   at each iteration of the algorithm.}
-#'
-#' \item{\code{line_search}}{Boolean indicating if backtracking line search
-#'   should be performed at each ccd iteration.}
-#'
-#' \item{\code{alpha}}{alpha value of line search between 0 and .5.}
-#'
-#' \item{\code{beta}}{beta value of line search between 0 and .5.}
-#'   
-#' \item{\code{calc_deriv}}{boolean indicated if maximum absolute derivatives of
-#'  \eqn{L} and \eqn{F} should be calculated at each step of optimization. This may
-#'  be useful for monitoring convergence though may have substantial computational
-#'  cost for large matrices.}
-#'   
-#' \item{\code{calc_max_diff}}{boolean indicating if maximum absolute difference 
-#' between successive updates of \eqn{L} and \eqn{F} should be calulcated and
-#' stored. This may be useful for monitoring convergence.}
-#'   
-#' }
-#'
-#' @param Y The n x p matrix of counts; all entries of Y should be
-#'   non-negative. Y may be a sparse matrix from the \code{Matrix}
-#'   package. 
-#'   
-#' @param K An integer 1 or greater giving the matrix rank. This
-#'   argument will be ignored if the initial fit
-#'   (\code{fit0}) is provided.
-#'   
-#' @param fit0 The initial model fit. It should be an object of class
-#'   \dQuote{glmpca_fit}, such as an output from \code{init_glmpca_pois}, 
-#'   or from a previous call to \code{fit_glmpca}.
-#'   
-#' @param tol Positive scalar determining relative tolerance for assessing convergence.
-#'   Convergence is determined by comparing the log-likelihood at the previous
-#'   iteration to the current iteration. 
-#'   
-#' @param min_iter Minimum number of updates to \eqn{L} and \eqn{F} to be run.
-#' 
-#' @param max_iter Maximum number of updates to \eqn{L} and \eqn{F} to be run.
-#' 
-#' @param verbose Boolean indicating if likelihood should be printed at each step.
-#'   
-#' @param control List of control parameters to modify behavior of \code{algorithm}.
-#' 
-#' @references
-#' Townes, F. W., Hicks, S. C., Aryee, M. J. and Irizarry,
-#' R. A. (2019). Feature selection and dimension reduction for
-#' single-cell RNA-Seq based on a multinomial model. \emph{Genome Biology}
-#' \bold{20}, 295. \url{https://doi.org/10.1186/s13059-019-1861-6}
-#'
-#' Collins, M., Dasgupta, S. and Schapire, R. E. (2002). A
-#' generalization of principal components analysis to the exponential
-#' family. In \emph{Advances in Neural Information Processing Systems} 14.
-#'
-#' @return An object capturing the final state of the model fit. It will contain
-#' the final values of \eqn{L} and \eqn{F}, as well as a dataframe \code{progress}
-#' that records information about the algorithm progress at each iteration.
-#'
-#' @import Matrix
-#' @importFrom utils modifyList
-#' @importFrom MatrixExtra mapSparse
-#' 
-#' @export
-#' 
-#' @examples 
-#' set.seed(1)
-#' 
-#' n <- 1000
-#' p <- 500
-#' K <- 1
-#' 
-#' data <- generate_glmpca_data_pois(n, p, K)
-#' # initialize fit
-#' fit0 <- init_glmpca_pois(
-#'   Y = data$Y, 
-#'   K = K,
-#'   fit_col_size_factor = TRUE, 
-#'   fit_row_intercept = TRUE
-#' )
-#' 
-#' fit <- fit_glmpca_pois(Y = data$Y, fit0 = fit0)
-#'
-fit_glmpca_pois <- function(
+aprox_ll_glmpca_pois <- function(
     Y, 
     K, 
     fit0, 
+    a1, 
+    a2,
     tol = 1e-4,
     min_iter = 1,
     max_iter = 100,
@@ -352,80 +243,45 @@ fit_glmpca_pois <- function(
       
     }
     
-    FF_T <- t(fit$FF)
     start_iter_LL <- fit$LL
       
     if (length(LL_update_indices) > 0) {
+            
+      for (i in 1:n) {
         
-        if (inherits(Y, "sparseMatrix")) {
-            
-            update_loadings_sp(
-              F_T = FF_T,
-              L = fit$LL,
-              Y_T = Y_T,
-              deriv_const_mat = -Matrix::crossprod(FF_T, Y_T),
-              update_indices = LL_update_indices,
-              num_iter = control$num_iter,
-              line_search = control$line_search,
-              alpha = control$alpha,
-              beta = control$beta,
-              ccd_iter_tol = control$ccd_iter_tol
-            )
-          
-        } else {
-            
-            update_loadings(
-              F_T = FF_T,
-              L = fit$LL,
-              Y_T = Y_T,
-              deriv_const_mat = -crossprod(FF_T, Y_T),
-              update_indices = LL_update_indices,
-              num_iter = control$num_iter,
-              line_search = control$line_search,
-              alpha = control$alpha,
-              beta = control$beta,
-              ccd_iter_tol = control$ccd_iter_tol
-            )
-          
-        }
+        fit$LL[, i] <- approx_reg_ccd(
+          X = t(fit$FF),
+          y = Y[i, ],
+          w = fit$LL[, i],
+          a1 = a1,
+          a2 = a2,
+          update_indices = LL_update_indices + 1,
+          n_updates = control$num_iter,
+          alpha = control$alpha,
+          beta = control$beta
+        )
+        
+      }
 
     }
     
-    LL_T <- t(fit$LL)
-    
     if (length(FF_update_indices) > 0) {
+            
+      for (j in 1:p) {
         
-        if (inherits(Y, "sparseMatrix")) {
-            
-            new_lik <- update_factors_sp(
-              L_T = LL_T,
-              FF = fit$FF,
-              Y = Y,
-              deriv_const_mat = -Matrix::crossprod(LL_T, Y),
-              update_indices = FF_update_indices,
-              num_iter = control$num_iter,
-              line_search = control$line_search,
-              alpha = control$alpha,
-              beta = control$beta,
-              ccd_iter_tol = control$ccd_iter_tol
-            ) - loglik_const
-          
-        } else {
-            
-          new_lik <- update_factors(
-            L_T = LL_T,
-            FF = fit$FF,
-            Y = Y,
-            deriv_const_mat = -crossprod(LL_T, Y),
-            update_indices = FF_update_indices,
-            num_iter = control$num_iter,
-            line_search = control$line_search,
-            alpha = control$alpha,
-            beta = control$beta,
-            ccd_iter_tol = control$ccd_iter_tol
-          ) - loglik_const
-          
-        }
+        fit$FF[, j] <- approx_reg_ccd(
+          X = t(fit$LL),
+          y = Y[, j],
+          w = fit$FF[, j],
+          a1 = a1,
+          a2 = a2,
+          update_indices = FF_update_indices + 1,
+          n_updates = control$num_iter,
+          alpha = control$alpha,
+          beta = control$beta
+        )
+        
+      }
 
     }
     
@@ -434,6 +290,13 @@ fit_glmpca_pois <- function(
     d[fixed_rows] <- 1
     fit$FF <- fit$FF * d
     fit$LL <- fit$LL / d
+    
+    new_lik <- do.call(
+      loglik_func,
+      list(
+        Y = Y, LL = fit$LL, FF = fit$FF, const = loglik_const
+      )
+    )
 
     if (new_lik >= current_lik && t >= min_iter) {
       
@@ -519,6 +382,367 @@ fit_glmpca_pois <- function(
     fit$progress$max_diff_FF <- fit$progress$max_diff_FF[1:t]
     fit$progress$max_diff_LL <- fit$progress$max_diff_LL[1:t] 
       
+  }
+  
+  rownames(fit$LL) <- LL_rownames
+  rownames(fit$FF) <- FF_rownames
+  
+  return(fit)
+  
+}
+
+fit_glmpca_pois <- function(
+    Y, 
+    K, 
+    fit0, 
+    tol = 1e-4,
+    min_iter = 1,
+    max_iter = 100,
+    verbose = TRUE,
+    control = list()
+) {
+  
+  verify.count.matrix(Y)
+  
+  if (!is.scalar(tol) || tol <= 0) {
+    
+    stop("Input argument \"tol\" must be a positive scalar")
+    
+  }
+  
+  if (min_iter > max_iter)
+    stop("\"min_iter\" must be less than or equal to \"max_iter\"")
+  
+  n <- nrow(Y)
+  p <- ncol(Y)
+  
+  control <- modifyList(
+    fit_glmpca_control_default(), 
+    control, 
+    keep.null = TRUE
+  )
+  
+  calc_deriv <- control$calc_deriv
+  calc_max_diff <- control$calc_max_diff
+  
+  if (missing(fit0)) {
+    
+    if (missing(K)) {
+      
+      stop("must provide either a \"fit0\" or a value of \"K\"")
+      
+    } else if (!is.scalar(K) || K < 1) {
+      
+      stop("\"K\" must be an integer greater than or equal to 1")
+      
+    }
+    
+    fit <- init_glmpca_pois(
+      Y = Y,
+      K = K
+    )
+    
+  } else {
+    
+    if (!inherits(fit0,"glmpca_fit"))
+      stop("Input argument \"fit0\" should be an object of class ",
+           "\"glmpca_fit\", such as an output of init_glmpca_pois")
+    
+    verify.fit(fit0)
+    fit <- fit0
+    
+  }
+  
+  LL_rownames <- rownames(fit$LL)
+  FF_rownames <- rownames(fit$FF)
+  
+  K <- nrow(fit$LL)
+  
+  # get update indices, subtracting 1 for C++ compatibility
+  LL_update_indices <- setdiff(1:K, fit$fixed_loadings) - 1
+  FF_update_indices <- setdiff(1:K, fit$fixed_factors) - 1
+  
+  Y_T <- Matrix::t(Y)
+  
+  # calculate part of log likelihood that doesn't change
+  if (!inherits(Y, "sparseMatrix")) {
+    
+    loglik_const <- sum(lfactorial(Y))
+    loglik_func <- lik_glmpca_pois_log
+    
+  } else {
+    
+    loglik_const <- sum(mapSparse(Y, lfactorial))
+    loglik_func <- lik_glmpca_pois_log_sp
+    
+  } 
+  
+  current_lik <- do.call(
+    loglik_func,
+    list(
+      Y = Y, LL = fit$LL, FF = fit$FF, const = loglik_const
+    )
+  )
+  
+  converged <- FALSE
+  
+  t <- 1
+  
+  if (verbose) {
+    cat(
+      sprintf(
+        "Fitting rank-%d GLM-PCA model to a %d x %d matrix.\n", K, n, p
+      )
+    )
+  }
+  
+  fit$progress <- list()
+  fit$progress[["iter"]] <- numeric(max_iter)
+  fit$progress[["loglik"]] <- numeric(max_iter)
+  fit$progress[["time"]] <- numeric(max_iter)
+  
+  if (calc_deriv) {
+    
+    fit$progress[["max_FF_deriv"]] <- numeric(max_iter)
+    fit$progress[["max_LL_deriv"]] <- numeric(max_iter)
+    
+    LL_mask <- matrix(
+      data = 1, nrow = nrow(fit$LL), ncol = ncol(fit$LL)
+    )
+    
+    LL_mask[fit$fixed_loadings, ] <- 0
+    if(!inherits(Y, "sparseMatrix")) {
+      
+      LL_mask <- t(LL_mask)
+      
+    }
+    
+    FF_mask <- matrix(
+      data = 1, nrow = nrow(fit$FF), ncol = ncol(fit$FF)
+    )
+    
+    FF_mask[fit$fixed_factors, ] <- 0
+    if(!inherits(Y, "sparseMatrix")) {
+      
+      FF_mask <- t(FF_mask)
+      
+    }
+    
+  }
+  
+  if (calc_max_diff) {
+    
+    fit$progress[["max_diff_FF"]] <- numeric(max_iter)
+    fit$progress[["max_diff_LL"]] <- numeric(max_iter)
+    
+  }
+  
+  fixed_rows <- union(fit$fixed_factors, fit$fixed_loadings)
+  
+  fit$progress$iter[1] <- 0
+  fit$progress$loglik[1] <- current_lik
+  fit$progress$time[1] <- 0
+  
+  if (calc_max_diff) {
+    
+    fit$progress$max_diff_FF[1] <- 0
+    fit$progress$max_diff_LL[1] <- 0
+    
+  }
+  
+  if(inherits(Y, "sparseMatrix") && calc_deriv) {
+    
+    fit$progress$max_FF_deriv[1] <- max(abs((deriv_product(fit$LL, fit$FF) - fit$LL %*% Y) * FF_mask))
+    fit$progress$max_LL_deriv[1] <- max(abs((deriv_product(fit$FF, fit$LL) - Matrix::tcrossprod(fit$FF, Y)) * LL_mask))
+    
+  } else if (calc_deriv) {
+    
+    fit$progress$max_FF_deriv[1] <- max(abs(crossprod(exp(crossprod(fit$LL, fit$FF)) - Y, t(fit$LL)) * FF_mask))
+    fit$progress$max_LL_deriv[1] <- max(abs(crossprod(exp(crossprod(fit$FF, fit$LL)) - Y_T, t(fit$FF)) * LL_mask))
+    
+  }
+  
+  while (!converged && t <= max_iter) {
+    
+    start_time <- Sys.time()
+    
+    if (verbose) {
+      
+      cat(
+        sprintf(
+          "Iteration %d: Log-Likelihood = %+0.8e\n", t-1, current_lik
+        )
+      )
+      
+    }
+    
+    FF_T <- t(fit$FF)
+    start_iter_LL <- fit$LL
+    
+    if (length(LL_update_indices) > 0) {
+      
+      if (inherits(Y, "sparseMatrix")) {
+        
+        update_loadings_sp(
+          F_T = FF_T,
+          L = fit$LL,
+          Y_T = Y_T,
+          deriv_const_mat = -Matrix::crossprod(FF_T, Y_T),
+          update_indices = LL_update_indices,
+          num_iter = control$num_iter,
+          line_search = control$line_search,
+          alpha = control$alpha,
+          beta = control$beta,
+          ccd_iter_tol = control$ccd_iter_tol
+        )
+        
+      } else {
+        
+        update_loadings(
+          F_T = FF_T,
+          L = fit$LL,
+          Y_T = Y_T,
+          deriv_const_mat = -crossprod(FF_T, Y_T),
+          update_indices = LL_update_indices,
+          num_iter = control$num_iter,
+          line_search = control$line_search,
+          alpha = control$alpha,
+          beta = control$beta,
+          ccd_iter_tol = control$ccd_iter_tol
+        )
+        
+      }
+      
+    }
+    
+    LL_T <- t(fit$LL)
+    
+    if (length(FF_update_indices) > 0) {
+      
+      if (inherits(Y, "sparseMatrix")) {
+        
+        new_lik <- update_factors_sp(
+          L_T = LL_T,
+          FF = fit$FF,
+          Y = Y,
+          deriv_const_mat = -Matrix::crossprod(LL_T, Y),
+          update_indices = FF_update_indices,
+          num_iter = control$num_iter,
+          line_search = control$line_search,
+          alpha = control$alpha,
+          beta = control$beta,
+          ccd_iter_tol = control$ccd_iter_tol
+        ) - loglik_const
+        
+      } else {
+        
+        new_lik <- update_factors(
+          L_T = LL_T,
+          FF = fit$FF,
+          Y = Y,
+          deriv_const_mat = -crossprod(LL_T, Y),
+          update_indices = FF_update_indices,
+          num_iter = control$num_iter,
+          line_search = control$line_search,
+          alpha = control$alpha,
+          beta = control$beta,
+          ccd_iter_tol = control$ccd_iter_tol
+        ) - loglik_const
+        
+      }
+      
+    }
+    
+    # rescale loadings and factors for numerical stability
+    d <- sqrt(abs(rowMeans(fit$LL)/rowMeans(fit$FF)))
+    d[fixed_rows] <- 1
+    fit$FF <- fit$FF * d
+    fit$LL <- fit$LL / d
+    
+    if (new_lik >= current_lik && t >= min_iter) {
+      
+      rel_improvement <- new_lik - current_lik
+      if (rel_improvement < tol) {
+        
+        converged <- TRUE
+        if (verbose) {
+          
+          cat(
+            sprintf(
+              "Iteration %d: Log-Likelihood = %+0.8e\n", t, new_lik
+            )
+          )
+          
+        }
+        
+      } 
+      
+    } 
+    
+    end_iter_time <- Sys.time()
+    time_since_start <- as.numeric(difftime(end_iter_time, start_time, units = "secs"))
+    current_lik <- new_lik
+    fit$progress$time[t + 1] <- time_since_start
+    fit$progress$iter[t + 1] <- t
+    fit$progress$loglik[t + 1] <- current_lik
+    
+    if (calc_max_diff) {
+      
+      fit$progress$max_diff_LL[t + 1] <- max(abs(fit$LL - start_iter_LL))
+      fit$progress$max_diff_FF[t + 1] <- max(abs(t(fit$FF) - FF_T))
+      
+    }
+    
+    if(inherits(Y, "sparseMatrix") && calc_deriv) {
+      
+      fit$progress$max_FF_deriv[t + 1] <- max(abs((deriv_product(fit$LL, fit$FF) - fit$LL %*% Y) * FF_mask))
+      fit$progress$max_LL_deriv[t + 1] <- max(abs((deriv_product(fit$FF, fit$LL) - Matrix::tcrossprod(fit$FF, Y)) * LL_mask))
+      
+    } else if (calc_deriv) {
+      
+      fit$progress$max_FF_deriv[t + 1] <- max(abs(crossprod(exp(crossprod(fit$LL, fit$FF)) - Y, t(fit$LL)) * FF_mask))
+      fit$progress$max_LL_deriv[t + 1] <- max(abs(crossprod(exp(crossprod(fit$FF, fit$LL)) - Y_T, t(fit$FF)) * LL_mask))
+      
+    }
+    
+    t <- t + 1
+    
+  }
+  
+  if (t > max_iter && !converged) {
+    
+    if (verbose) {
+      
+      cat(
+        sprintf(
+          "Iteration %d: Log-Likelihood = %+0.8e\n", t - 1, new_lik
+        )
+      )
+      
+    }
+    
+    warning(
+      sprintf("Algorithm reached maximum iterations without convergence.")
+    )
+    
+  }
+  
+  fit$progress$time <- fit$progress$time[1:t]
+  fit$progress$iter <- fit$progress$iter[1:t]
+  fit$progress$loglik <- fit$progress$loglik[1:t]
+  
+  if (calc_deriv) {
+    
+    fit$progress$max_FF_deriv <- fit$progress$max_FF_deriv[1:t]
+    fit$progress$max_LL_deriv <- fit$progress$max_LL_deriv[1:t]
+    
+  }
+  
+  if (calc_max_diff) {
+    
+    fit$progress$max_diff_FF <- fit$progress$max_diff_FF[1:t]
+    fit$progress$max_diff_LL <- fit$progress$max_diff_LL[1:t] 
+    
   }
   
   rownames(fit$LL) <- LL_rownames
---
+++ R/taylor_approx_ll.R
@@ -0,0 +1,89 @@
+approx_reg_ccd <- function(
+  X,
+  y,
+  w,
+  a1,
+  a2,
+  update_indices,
+  n_updates,
+  alpha,
+  beta
+) {
+  
+  zero_idx <- which(y == 0)
+  non_zero_idx <- which(y != 0)
+  num_zero <- length(zero_idx)
+  y_nz <- y[non_zero_idx]
+  X_0 <- X[zero_idx, ]
+  X_nz <- X[non_zero_idx, ]
+  Z_0 <- crossprod(X_0)
+  m_0 <- crossprod(X_0[, update_indices], rep(-a1, num_zero))
+  m_nz <- crossprod(X_nz[, update_indices], y_nz)
+  m <- m_0 + m_nz
+  sqrd_factor <- 2 * a2
+  
+  p <- length(w)
+  
+  eta <- X_nz %*% w
+  exp_eta <- exp(eta)
+  
+  current_nonlinear_lik <- a2 * tcrossprod(w %*% Z_0, w) + sum(exp_eta)
+  
+  for (updates in 1:n_updates) {
+    
+    for (idx in 1:length(update_indices)) {
+      
+      j = update_indices[idx]
+      
+      current_lik <- current_nonlinear_lik - w[j] * m[idx]
+      deriv_prod <- exp_eta * X_nz[, j]
+      first_deriv <- sqrd_factor * sum(w * Z_0[,j]) + sum(deriv_prod) - m[idx]
+      second_deriv <- sqrd_factor * Z_0[j, j] + sum(deriv_prod * X_nz[, j])
+      
+      newton_dir = first_deriv / second_deriv
+      
+      t = 1.0
+      step_accepted = FALSE
+      newton_dec = alpha * first_deriv * newton_dir
+      w_j_og = w[j]
+      eta_og = eta
+      exp_eta_og = exp_eta
+      
+      while(!step_accepted) {
+        
+        w[j] = w_j_og - t * newton_dir
+        eta_proposed = eta + (w[j] - w_j_og) * X_nz[, j]
+        exp_eta_proposed = exp(eta_proposed)
+        f_proposed = a2 * tcrossprod(w %*% Z_0, w) + sum(exp_eta_proposed) - w[j] * m[idx]
+        
+        if (f_proposed <= current_lik - t * newton_dec) {
+          
+          step_accepted = TRUE
+          current_nonlinear_lik = f_proposed + w[j] * m[idx]
+          eta = eta_proposed
+          exp_eta = exp_eta_proposed
+          
+        } else {
+          
+          t = beta * t
+          if (abs((t * newton_dir) / w_j_og) < 1e-16) {
+            
+            w[j] = w_j_og
+            step_accepted = TRUE
+            eta = eta_og
+            exp_eta = exp_eta_og
+            
+          }
+          
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  return(w)
+  
+}
+
