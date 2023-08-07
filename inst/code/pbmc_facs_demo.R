c120c6e61630aa469543840c3a6d52645721d584
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-08-07 14:51:27 -0500

    Bumped up version.

--- inst/code/pbmc_facs_demo.R
+++ inst/code/pbmc_facs_demo.R
@@ -4,6 +4,5 @@ library(cowplot)
 set.seed(1)
 data(pbmc_facs)
 Y <- pbmc_facs$counts
-fit0 <- init_glmpca_pois(Y,K = 2)
-stop()
-fit <- fit_glmpca_pois(Y,K = 2)
+fit <- fit_glmpca_pois(Y,K = 3,max_iter = 4)
+range(with(fit,crossprod(LL,FF)) - with(fit2,tcrossprod(U %*% D,V)))
