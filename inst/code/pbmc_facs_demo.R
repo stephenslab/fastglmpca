d8b9977037e2b921d3706ca61837ee13d8d8891d
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-08-16 15:36:54 -0500

    A few updates to the pbmc_facs_demo.R test script.

--- inst/code/pbmc_facs_demo.R
+++ inst/code/pbmc_facs_demo.R
@@ -4,11 +4,12 @@ library(cowplot)
 set.seed(1)
 data(pbmc_facs)
 Y <- as.matrix(pbmc_facs$counts)
+n <- nrow(Y)
+m <- ncol(Y)
 fit0_rank1 <- init_glmpca_pois(Y,K = 1)
+fit0_init <- init_glmpca_pois(Y,
+                              U = matrix(rnorm(3*n),n,3),
+                              V = matrix(rnorm(3*m),m,3))
 fit0 <- init_glmpca_pois(Y,K = 3)
-stop()
-crossprod(fit0$U)
-crossprod(fit0$V)
-fit0$D
-fit <- fit_glmpca_pois(Y,K = 3,max_iter = 4)
+# fit <- fit_glmpca_pois(Y,K = 3,max_iter = 4)
 
