41cb1a7b96d67d1f616cf5f2235f6ea610626c27
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-09-28 10:05:50 -0500

    Added tests for orthonormalize = FALSE.

--- tests/testthat/test_fit_glmpca_pois.R
+++ tests/testthat/test_fit_glmpca_pois.R
@@ -9,9 +9,9 @@ test_that("Some basic tests of fit_glmpca_pois",{
   Y <- generate_glmpca_data_pois(n,m,K = 3)$Y
 
   # Fit a GLM-PCA model to the data.
-  fit0 <- init_glmpca_pois(Y,K = 3)
+  fit0 <- init_glmpca_pois(Y,K = 3,)
   suppressWarnings(capture.output(
-    fit <- fit_glmpca_pois(Y,fit0 = fit0,
+    fit <- fit_glmpca_pois(Y,fit0 = fit0,max_iter = 500,tol = 1e-8,
                            control = list(calc_deriv = TRUE,
                                           calc_max_diff = TRUE))))
   capture.output(print(summary(fit)))
@@ -43,7 +43,7 @@ test_that("fit_glmpca_pois works with K = 1",{
   # Fit a GLM-PCA model to the data.
   fit0 <- init_glmpca_pois(Y,K = 1)
   suppressWarnings(capture.output(
-    fit <- fit_glmpca_pois(Y,fit0 = fit0,
+    fit <- fit_glmpca_pois(Y,fit0 = fit0,max_iter = 500,tol = 1e-8,
                            control = list(calc_deriv = TRUE,
                                           calc_max_diff = TRUE))))
   capture.output(print(summary(fit)))
@@ -60,4 +60,37 @@ test_that("fit_glmpca_pois works with K = 1",{
   # Check that orthogonality constraints are satisfied.
   expect_equivalent(crossprod(fit$U),1,scale = 1,tolerance = 1e-8)
   expect_equivalent(crossprod(fit$V),1,scale = 1,tolerance = 1e-8)
+})
+
+test_that("fit_glmpca_pois works with orthonormalize = FALSE",{
+
+  # Simulate a 100 x 200 data set to factorize.
+  set.seed(1)
+  n <- 100
+  m <- 200
+  Y <- generate_glmpca_data_pois(n,m,K = 3)$Y
+
+  # Fit a GLM-PCA model to the data.
+  fit0 <- init_glmpca_pois(Y,K = 3)
+  suppressWarnings(capture.output(
+    fit <- fit_glmpca_pois(Y,fit0 = fit0,max_iter = 500,tol = 1e-8,
+                           control = list(orthonormalize = FALSE,
+                                          calc_deriv = TRUE,
+                                          calc_max_diff = TRUE))))
+  capture.output(print(summary(fit)))
+
+  # All the updates should monotonically increase the likelihood.
+  expect_nondecreasing(fit$progress$loglik)
+
+  # The fixed row and column intercepts should not change.
+  expect_equal(fit0$X,fit$X)
+  expect_equal(fit0$B,fit$B)
+  expect_equal(fit0$Z,fit$Z)
+  expect_equal(fit0$W,fit$W)
+
+  # Check that orthogonality constraints are satisfied.
+  d <- diag(fit$D)
+  expect_equivalent(crossprod(fit$U),diag(3),scale = 1,tolerance = 1e-8)
+  expect_equivalent(crossprod(fit$V),diag(3),scale = 1,tolerance = 1e-8)
+  expect_equivalent(d,sort(d,decreasing = TRUE))
 })
