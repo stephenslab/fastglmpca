d3da51dba48facb979f84832b79b7bb06d838df6
Eric Weine <ericweine15@gmail.com>
2023-10-17 12:49:51 -0400

    removed openmp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,4 @@
 #include <RcppArmadillo.h>
-#include <omp.h>
 #include <Rcpp.h>
 using namespace arma;
 using namespace Rcpp;
@@ -7,7 +6,6 @@ using namespace Rcpp;
 // L is K x n
 // F is K x p
 // [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::plugins(openmp)]]
 // [[Rcpp::export]]
 double big_exp_crossprod(
   const arma::mat& L,
@@ -15,26 +13,24 @@ double big_exp_crossprod(
   const int n,
   const int p
 ) {
-  
+
   double sum = 0.0;
-  
+
   // First, get this code to work without parallelism
   // Then, I can see if that would help
   for (int i = 0; i < n; i++) {
-    
-    #pragma omp parallel for reduction (+:sum)
+
     for (int j = 0; j < p; j++) {
-      sum = sum + exp(arma::dot(L.col(i), F.col(j)));      
+      sum = sum + exp(arma::dot(L.col(i), F.col(j)));
     }
   }
   return(sum);
-  
+
 }
 
 // L is K x n
 // F is K x p
 // [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::plugins(openmp)]]
 // [[Rcpp::export]]
 double big_elementwise_mult_crossprod(
     const arma::mat& L,
@@ -44,38 +40,35 @@ double big_elementwise_mult_crossprod(
     const std::vector<int> nonzero_y_j_idx,
     const int num_nonzero_y
 ) {
-  
+
   double sum = 0.0;
-  
-  #pragma omp parallel for reduction (+:sum)
+
   for (int r = 0; r < num_nonzero_y; r++) {
     sum = sum + nonzero_y[r] * arma::dot(L.col(nonzero_y_i_idx[r]), F.col(nonzero_y_j_idx[r]));
-    
+
   }
-  
+
   return(sum);
-  
+
 }
 
 // L is K x n
 // F is K x p
 // compute L %*% exp(t(L) %*% F)
 // [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::plugins(openmp)]]
 // [[Rcpp::export]]
 arma::mat deriv_product(
     const arma::mat& L,
     const arma::mat& F
 ) {
-  
+
   const int n = L.n_cols;
   const int K = L.n_rows;
   const int p = F.n_cols;
   arma::mat prod;
   prod.zeros(K, p);
   double exp_arg;
-  
-  #pragma omp parallel for shared(prod) private(exp_arg)
+
   for (int k = 0; k < n; k++) {
     for (int i = 0; i < K; i++) {
       for (int j = 0; j < p; j++) {
@@ -83,13 +76,12 @@ arma::mat deriv_product(
         for (int m = 0; m < K; m++) {
           exp_arg += L(m, k) * F(m, j);
         }
-        
-        #pragma omp atomic
+
         prod(i, j) += L(i, k) * exp(exp_arg);
       }
     }
   }
-  
+
   return(prod);
-  
+
 }
