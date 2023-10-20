defc1fd1119554e9246e4c4c5bfe77217ad3d437
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-10-20 15:50:23 -0500

    Implemented new function set_fastglmpca_threads.

---
+++ R/threads.R
@@ -0,0 +1,24 @@
+#' @title Set up Multithreading for fastglmpca
+#' 
+#' @description Initialize RcppParallel multithreading using a
+#'   pre-specified number of threads, or using the default number of
+#'   threads when \code{n} is not specified or is NA.
+#'
+#' @param n The requested number of threads.
+#'
+#' @return The number of threads to be used.
+#' 
+#' @importFrom RcppParallel setThreadOptions
+#' @importFrom RcppParallel defaultNumThreads
+#'
+#' @export
+#' 
+set_fastglmpca_threads <- function (n) {
+  if (missing(n) | is.na(n)) {
+    setThreadOptions()
+    n <- defaultNumThreads()
+  } else
+    setThreadOptions(numThreads = n)
+  message(sprintf("Using %d RcppParallel threads.",n))
+  return(n)
+}
