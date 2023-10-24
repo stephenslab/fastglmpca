97e2f9822fe4dd14624fff7b3bbf16252667271e
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-10-24 11:39:12 -0500

    A few changes to the benchmarking script.

--- inst/code/sparse_benchmark.R
+++ inst/code/sparse_benchmark.R
@@ -1,14 +1,19 @@
 # TO DO: Explain here what this script is for, and how to use it.
+#
+#   source activate base
+#   export MEM_CHECK_INTERVAL=0.01
+#   python3 monitor_memory.py Rscript sparse_benchmark.R
+#
 # library(fastglmpca)
 devtools::load_all()
 library(Matrix)
 load("../datasets/newsgroups.RData")
-counts <- counts[1:4000,1:1e4]
-topics <- topics[1:4000]
+counts <- counts[1:6000,1:2e4]
+topics <- topics[1:6000]
 # counts <- as.matrix(counts)
+print(format(object.size(counts),unit = "Mb"))
 t0 <- proc.time()
 fit <- fit_glmpca_pois(counts,K = 3,max_iter = 10,
-                       control = list(calc_max_diff = TRUE,
-                                       calc_deriv = TRUE))
+                       control = list(calc_max_diff = TRUE,calc_deriv = TRUE))
 t1 <- proc.time()
 print(t1 - t0)
