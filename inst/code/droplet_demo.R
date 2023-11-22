820a042943a86e69f121e393a8206354ec54a4b9
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-11-21 22:26:56 -0600

    A few adjustments to the droplet_demo.R script.

--- inst/code/droplet_demo.R
+++ inst/code/droplet_demo.R
@@ -1,8 +1,11 @@
 # sinteractive -p mstephens --account=pi-mstephens --time=24:00:00 \
 #   -c 10 --mem=16G
-line_search    <- TRUE
-use_daarem     <- FALSE
-orthonormalize <- TRUE
+# use_daarem     <- FALSE
+# orthonormalize <- TRUE
+# outfile        <- "droplet_k=5_daarem=no.RData"
+use_daarem     <- TRUE
+orthonormalize <- FALSE
+outfile        <- "droplet_k=5_daarem=yes.RData"
 library(Matrix)
 library(RhpcBLASctl)
 library(fastglmpca)
@@ -12,17 +15,15 @@ omp_set_num_threads(10)
 blas_set_num_threads(1)
 set.seed(1)
 counts <- t(counts)
-cat("line_search    =",line_search,"\n")
 cat("use_daarem     =",use_daarem,"\n")
 cat("orthonormalize =",orthonormalize,"\n")
+cat("outfile        =",outfile,"\n")
 fit0 <- fit_glmpca_pois(counts,K = 5,
-                        control = list(maxiter = 10,tol = 0,
-                                       line_search = TRUE,
+                        control = list(maxiter = 100,tol = 0,
                                        use_daarem = FALSE,
                                        orthonormalize = TRUE))
 fit <- fit_glmpca_pois(counts,fit0 = fit0,
-                       control = list(maxiter = 10,tol = 0,
-                                      line_search = line_search,
+                       control = list(maxiter = 100,tol = 0,
                                       use_daarem = use_daarem,
                                       orthonormalize = orthonormalize))
-save(list = "fit",file = "droplet_k=5_daarem=no.RData")
+save(list = "fit",file = outfile)
