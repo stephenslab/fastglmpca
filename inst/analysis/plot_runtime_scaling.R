bade15b405799e14cefcf086833c444c1e536ede
Eric Weine <ericw456@mit.edu>
2024-01-18 13:32:03 -0500

    added code for figures

--- inst/analysis/plot_runtime_scaling.R
+++ inst/analysis/plot_runtime_scaling.R
@@ -14,6 +14,32 @@ df_long <- data.frame(
 
 library(ggplot2)
 
-ggplot(data = df_long) +
+g1 <- ggplot(data = df_long) +
+  geom_point(aes(x = ncells, y = time, color = algorithm)) +
   geom_line(aes(x = ncells, y = time, color = algorithm)) +
-  cowplot::theme_cowplot()
+  cowplot::theme_cowplot() +
+  xlab("Number of Cells") +
+  ylab("Time (s)")
+
+
+df <- readr::read_rds(
+  "~/Documents/data/fastglmpca/experiment_results/num_factors_scaling_df.rds"
+)
+
+df_long <- data.frame(
+  K = rep(df$K, 3),
+  time = c(df$fastglmpca_time, df$glmpca_time, df$scGBM_time),
+  algorithm = c(
+    rep("fastglmpca", nrow(df)),
+    rep("glmpca-sgd", nrow(df)),
+    rep("scGBM", nrow(df))
+  )
+)
+
+g2 <- ggplot(data = df_long) +
+  geom_point(aes(x = K, y = time, color = algorithm)) +
+  geom_line(aes(x = K, y = time, color = algorithm)) +
+  cowplot::theme_cowplot() +
+  xlab("Number of Factors") +
+  ylab("Time (s)")
+
