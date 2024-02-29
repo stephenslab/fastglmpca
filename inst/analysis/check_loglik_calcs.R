e4d753711a23e951b66131b178988734ab9bc78a
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-02-29 15:13:13 -0600

    Revised the functions in inst/code/loglik.R so that they output the log-rates.

--- inst/analysis/check_loglik_calcs.R
+++ inst/analysis/check_loglik_calcs.R
@@ -1,4 +1,5 @@
-# TO DO: Explain what this script is for.
+# A script to check our log-likelihood calculations for different
+# packages (glmpca, scGBM, fastglmpca).
 library(Matrix)
 library(fastTopics)
 source("../code/loglik.R")
@@ -9,10 +10,9 @@ fit1 <- readRDS(paste("droplets_fastglmpca_fit_10_factors_5105_iter_28",
 fit2 <- readRDS(paste("droplets_glmpca_fit_10_factors_10_hrs_avagrad",
                       "optimizer_minibatch_stochastic_dec_23.rds",sep = "_"))
 fit3 <- readRDS("droplets_scGBM_fit_10_factors_no_beta_infer_10_hrs.rds")
-ll1 <- loglik_fastglmpca(fit1,counts)
-ll2 <- loglik_glmpca(fit2,counts)
-ll3 <- loglik_scgbm(fit3,counts)
-ll_const <- sum(lfactorial(as.matrix(counts)))
+ll1 <- glmpca_pois_loglik(counts,logrates_fastglmpca(fit1))
+ll2 <- glmpca_pois_loglik(counts,logrates_glmpca(fit2))
+ll3 <- glmpca_pois_loglik(counts,logrates_scgbm(fit3),const = 0)
 print((fit1$loglik - ll1)/ll1)
 print((max(fit2$lik) - ll2)/ll2)
-print((max(fit3$loglik - ll_const) - ll3)/ll3)
+print((max(fit3$loglik) - ll3)/ll3)
--- inst/code/loglik.R
+++ inst/code/loglik.R
@@ -1,35 +1,32 @@
-# TO DO: Explain here what this function does, and how to use it.
-loglik_fastglmpca <- function (fit, Y, e = 1e-8) {
-  Y <- as.matrix(Y)
-  a <- sum(lfactorial(Y))
-  H <- with(fit,
-            tcrossprod(U %*% diag(d),V) +
-            tcrossprod(X,B) +
-            tcrossprod(W,Z))
-  return(sum(Y*H - exp(H)) - a)
+# Compute the log-likelihood for
+#
+#   y_ij ~ Pois(h_ij)
+#
+glmpca_pois_loglik <- function (Y, H, const = sum(lfactorial(as.matrix(Y))))
+  sum(Y*H - exp(H)) - const
+
+# Compute the log-rates from a fastglmpca output.
+logrates_fastglmpca <- function (fit)
+  with(fit,
+       tcrossprod(U %*% diag(d),V) +
+       tcrossprod(X,B) +
+       tcrossprod(W,Z))
+
+# Compute the log-rates from a glmpca output, assuming coefZ is NULL.
+logrates_glmpca <- function (fit) {
+  n <- nrow(fit$loadings)
+  return(with(fit,
+              tcrossprod(as.matrix(coefX),as.matrix(X)) +
+              tcrossprod(rep(1,n),offsets) +
+              tcrossprod(as.matrix(loadings),as.matrix(factors))))
 }
 
-# TO DO: Explain here what this function does, and how to use it.
-loglik_glmpca <- function (fit, Y, e = 1e-8) {
-  Y <- as.matrix(Y)
-  n <- nrow(Y)
-  a <- sum(lfactorial(Y))
-  H <- with(fit,
-            tcrossprod(as.matrix(coefX),as.matrix(X)) +
-            tcrossprod(rep(1,n),offsets) +
-            tcrossprod(as.matrix(loadings),as.matrix(factors)))
-  return(sum(Y*H - exp(H)) - a)
-}
-
-# TO DO: Explain here what this function does, and how to use it.
-loglik_scgbm <- function (fit, Y, e = 1e-8) {
-  Y <- as.matrix(Y)
-  n <- nrow(Y)
-  m <- ncol(Y)
-  a <- sum(lfactorial(Y))
-  H <- with(fit,
-            tcrossprod(U %*% diag(D),V) +
-            tcrossprod(alpha,rep(1,m)) +
-            tcrossprod(rep(1,n),beta))
-  return(sum(Y*H - exp(H)) - a)
+# Compute thek log-rates from a scGBM output.
+logrates_scgbm <- function (fit) {
+  n <- nrow(fit$U)
+  m <- nrow(fit$V)
+  return(with(fit,
+              tcrossprod(U %*% diag(D),V) +
+              tcrossprod(alpha,rep(1,m)) +
+              tcrossprod(rep(1,n),beta)))
 }
