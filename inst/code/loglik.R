3546be808bd5e89de096d5f0ed55e94ea5eab417
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-02-29 14:55:07 -0600

    I have a first draft of check_loglik_calcs.R

--- inst/code/loglik.R
+++ inst/code/loglik.R
@@ -8,3 +8,28 @@ loglik_fastglmpca <- function (fit, Y, e = 1e-8) {
             tcrossprod(W,Z))
   return(sum(Y*H - exp(H)) - a)
 }
+
+# TO DO: Explain here what this function does, and how to use it.
+loglik_glmpca <- function (fit, Y, e = 1e-8) {
+  Y <- as.matrix(Y)
+  n <- nrow(Y)
+  a <- sum(lfactorial(Y))
+  H <- with(fit,
+            tcrossprod(as.matrix(coefX),as.matrix(X)) +
+            tcrossprod(rep(1,n),offsets) +
+            tcrossprod(as.matrix(loadings),as.matrix(factors)))
+  return(sum(Y*H - exp(H)) - a)
+}
+
+# TO DO: Explain here what this function does, and how to use it.
+loglik_scgbm <- function (fit, Y, e = 1e-8) {
+  Y <- as.matrix(Y)
+  n <- nrow(Y)
+  m <- ncol(Y)
+  a <- sum(lfactorial(Y))
+  H <- with(fit,
+            tcrossprod(U %*% diag(D),V) +
+            tcrossprod(alpha,rep(1,m)) +
+            tcrossprod(rep(1,n),beta))
+  return(sum(Y*H - exp(H)) - a)
+}
